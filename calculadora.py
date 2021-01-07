3dd3eb7e43e2f833ed6a48cabefe20c510d43ce5
AbaddooN <77032892+AbaddooN@users.noreply.github.com>
2021-01-07 11:35:50 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,10 @@
 while True:
  operacao = input('Qual operacao (+,-,*,/) deseja fazer ?: ')
- num1 = int(input('Digite o primeiro numero: '))
- num2 = int(input('Digite o segundo numero: '))
-
+ if operacao == '+' or operacao == '-' or operacao == '*' or operacao == '/':
+  num1 = int(input('Digite o primeiro numero: '))
+  num2 = int(input('Digite o segundo numero: '))
+ else:
+   print('Operacao Invalida')
  if operacao == '+':
    total = num1 + num2
    print(total)
