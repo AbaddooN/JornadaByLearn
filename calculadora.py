1a92d48bd72d9e355e518e88e677721b3c3ca038
AbaddooN <77032892+AbaddooN@users.noreply.github.com>
2021-01-07 00:00:49 -0300

    Add files via upload
    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+while True:
+ operacao = input('Qual operacao (+,-,*,/) deseja fazer ?: ')
+ num1 = int(input('Digite o primeiro numero: '))
+ num2 = int(input('Digite o segundo numero: '))
+
+ if operacao == '+':
+   total = num1 + num2
+   print(total)
+ elif operacao == '*':
+   total = num1 * num2
+   print(total)
+ elif operacao == '/':
+   total = num1 / num2
+   print(total)
+ elif operacao == '-':
+   total = num1 - num2
+   print:(total)
